8e9f390a4b337d33406e551f9df8644ba257cbc2
Sofya Inglikova <sofiainglikova@gmail.com>
2021-12-16 21:14:03 +0300

    deploy problems fixed

---
+++ src/main/java/com/example/web4back/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.web4back;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Web4backApplication.class);
+    }
+
+}
